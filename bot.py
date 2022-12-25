03cfac5928b1fa5c45627a2a5617bece6cec041e
KingPhoenix06 <87867098+KingPhoenix06@users.noreply.github.com>
2022-12-25 23:59:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,13 +2,13 @@ import asyncio
 from os import environ
 from pyrogram import Client, filters, idle
 
-API_ID = int(environ.get("API_ID"))
-API_HASH = environ.get("API_HASH")
-BOT_TOKEN = environ.get("BOT_TOKEN")
-SESSION = environ.get("SESSION")
-TIME = int(environ.get("TIME"))
+API_ID = int(environ.get("API_ID","3369707"))
+API_HASH = environ.get("API_HASH","aec1fd7abdfec322c426961a570ef336")
+BOT_TOKEN = environ.get("BOT_TOKEN","5756617712:AAFoOcwTm8SmBkNQEBk79CqXROTMwQGv8II")
+SESSION = environ.get("SESSION","BQByDjHneo7z7A3PGQzlnBqJT8Flfh_CAjPvpHaCrZAZld_Kb1C6abM3K65Nsy0w19Ax9DhfkpOmRxSS0R0lEZGaoWIcdCYwnUBIUxhGNo_v-FdZarQjuNsR9K1rQmzXSn9hoNDJG9Sp7ILZW6Nn3ncUvU3c5KK41uPGuENKDGzoHZ5y7lPV0QE7VtWYWt9nW5sfopv2m9dICSX2ikzWPRFGJqO35LdHnzeqSNAJd8UxqEtDS5KnjKEhoab-ugKZV1A-KLC115WVLAY6lF_8KUFPzaqIIG-b_-tu7zvNhbEdYEe58lScCgk34UMOhUP8yrEgrcH7ZJPUbpiPfh6YZJzRQnZcZwA")
+TIME = int(environ.get("TIME","300"))
 GROUPS = []
-for grp in environ.get("GROUPS").split():
+for grp in environ.get("GROUPS",'-1001805750891').split():
     GROUPS.append(int(grp))
 ADMINS = []
 for usr in environ.get("ADMINS").split():
